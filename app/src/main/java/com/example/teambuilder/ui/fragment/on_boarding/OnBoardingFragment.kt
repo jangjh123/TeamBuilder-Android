4396608f7912a7e84e782ea23e217d8c21040a0b
gn000123 <gn000123@naver.com>
2022-07-31 17:14:39 +0900

    [remove] 불필요한 Fragment 삭제

--- app/src/main/java/com/example/teambuilder/ui/fragment/on_boarding/OnBoardingFragment.kt
+++
@@ -1,8 +0,0 @@
-package com.example.teambuilder.ui.fragment.on_boarding
-
-import com.example.teambuilder.R
-import com.example.teambuilder.databinding.FragmentOnBoardingBinding
-import com.example.teambuilder.ui.BaseFragment
-
-class OnBoardingFragment : BaseFragment<FragmentOnBoardingBinding>(R.layout.fragment_on_boarding) {
-}

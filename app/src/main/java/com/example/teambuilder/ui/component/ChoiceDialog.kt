5ba316f062d2d2d220c779171921caecba2b8a2c
gn000123 <gn000123@naver.com>
2022-08-01 00:13:23 +0900

    [feat] TeamBuildFragment UI 구현 중

---
+++ app/src/main/java/com/example/teambuilder/ui/component/ChoiceDialog.kt
@@ -0,0 +1,50 @@
+package com.example.teambuilder.ui.component
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.DialogFragment
+import com.example.teambuilder.R
+import com.example.teambuilder.data.model.Player
+import com.example.teambuilder.databinding.DialogChoiceBinding
+
+class ChoiceDialog(
+    private val players: List<Player>,
+    private val title: String,
+    private inline val onChosen: (Player) -> Unit
+) : DialogFragment() {
+
+    private lateinit var binding: DialogChoiceBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.dialog_choice, container, false)
+        isCancelable = false
+        binding.root.background = ColorDrawable(Color.TRANSPARENT)
+        initView()
+
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        setBackgroundTransparent()
+    }
+
+    private fun setBackgroundTransparent() {
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+    }
+
+    private fun initView() {
+        binding.tvTitle.text = title
+
+    }
+}
--- app/src/main/java/com/example/teambuilder/ui/fragment/team_build/TeamBuildFragment.kt
+++ app/src/main/java/com/example/teambuilder/ui/fragment/team_build/TeamBuildFragment.kt
@@ -1,18 +1,51 @@
 package com.example.teambuilder.ui.fragment.team_build
 
+import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.AlphaAnimation
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModel
 import com.example.teambuilder.R
 import com.example.teambuilder.databinding.FragmentTeamBuildBinding
 import com.example.teambuilder.ui.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class TeamBuildFragment : BaseFragment<FragmentTeamBuildBinding>(R.layout.fragment_team_build) {
     private val viewModel: TeamBuildViewModel by viewModels()
 
     override fun proceed() {
-        viewModel.foo()
+        binding.fragment = this@TeamBuildFragment
+        startAnim()
+    }
+
+    private fun startAnim() {
+        val fadeIn1 = AlphaAnimation(0f, 1f).apply {
+            interpolator = AccelerateDecelerateInterpolator()
+            duration = 1000
+        }
+
+        val fadeIn2 = AlphaAnimation(0f, 0.4f).apply {
+            interpolator = AccelerateDecelerateInterpolator()
+            fillAfter = true
+            startOffset = 1000
+            duration = 1500
+        }
+
+        binding.textView3.startAnimation(fadeIn1)
+        binding.btnTeamALeader.startAnimation(fadeIn1)
+        binding.btnTeamBLeader.startAnimation(fadeIn1)
+
+        binding.textView4.startAnimation(fadeIn2)
+        binding.rgMemberSelectWay.startAnimation(fadeIn2)
+    }
+
+    fun setTeamALeader(view: View) {
+
+    }
+
+    fun setTeamBLeader(view: View) {
+
     }
 
     override fun setObserver() {

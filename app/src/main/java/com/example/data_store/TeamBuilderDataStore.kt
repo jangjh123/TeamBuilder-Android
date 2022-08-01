bf9de135c8b4998f294125a4b66ad2a9de87ef65
gn000123 <gn000123@naver.com>
2022-08-02 01:22:11 +0900

    [feat] DataStore 재구현
    경기 예정 여부를 저장하기 위한 DataStore 를 재구현함.

---
+++ app/src/main/java/com/example/data_store/TeamBuilderDataStore.kt
@@ -0,0 +1,10 @@
+package com.example.data_store
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "team_builder")
+val KEY_IS_EXIST = booleanPreferencesKey("is_exist")
